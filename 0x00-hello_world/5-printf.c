e00f67191f5a95ec970d5b4aaab08ffe0e3abf5a
Mbccode <bmkpume@yahoo.com>
2023-07-13 16:53:10 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: Returns 0 if exited in proper manner
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
