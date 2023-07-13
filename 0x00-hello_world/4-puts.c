08dc6ad46dbefb18bf54d784d061725703c59355
Mbccode <bmkpume@yahoo.com>
2023-07-13 10:28:08 -0700

    First C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * Main: Entry point
+ *
+ * Return: Returns the value of 0
+ * 	   it als shows EOF.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
