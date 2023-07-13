ad8735d828ae1342f1315cca8f9c809b4702c6ee
Mbccode <bmkpume@yahoo.com>
2023-07-13 10:35:41 -0700

    First C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  * Main: Entry point
  *
- * Return: Returns the value of 0
- * 	   it als shows EOF.
+ * Return: Returns the value of 0 it als shows EOF.
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
