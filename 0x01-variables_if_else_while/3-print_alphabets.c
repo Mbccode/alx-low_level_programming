0b9d15160b5b2cc08988e2f38c74cfcc01fee5a9
Mbccode <bmkpume@yahoo.com>
2023-07-14 05:29:03 -0700

    Upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Use putchar to output lowercase and uppercase letters
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
