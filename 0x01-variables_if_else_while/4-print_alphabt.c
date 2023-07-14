f0e06d80b7fac9fa4fa968862296d43ad0194b40
Mbccode <bmkpume@yahoo.com>
2023-07-14 05:43:06 -0700

    Not q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ * Description: Prints all letters except q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z' != 'q' && 'e')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
