6116ad059264f3a0ecac26e2f5a15d7e6fefef69
Mbccode <bmkpume@yahoo.com>
2023-07-14 06:43:33 -0700

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Number in reverse order
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
