d0106ff8a55ebb0688120b340badaddabd20ae43
Mbccode <bmkpume@yahoo.com>
2023-07-18 03:11:31 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if the function call is correct
+ */
+
+void _putchar1(void)
+{
+	printf("_putchar");
+}
+
+int main(void)
+{
+	_putchar1();
+	return 0;
+}
