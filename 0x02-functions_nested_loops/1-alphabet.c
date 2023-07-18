1a4a1b459cd4f4377e0911db480121c4a10e66a6
Mbccode <bmkpume@yahoo.com>
2023-07-18 10:53:01 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+
+void print_alphabet(void)
+{
+	char ben;
+
+	for (ben = 'a'; ben <= 'z'; ben++)
+		_putchar(ben);
+	_putchar('\n');
+}
