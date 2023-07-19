7681b2e5afa89d3f8413fa664382b0b9e783d8a1
Mbccode <bmkpume@yahoo.com>
2023-07-18 18:31:08 -0700

    10-ad

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returnsthe result
+ *
+ * @x: integer being added
+ *
+ * @y: integer being added
+ *
+ * Return: Always 0 on success
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
