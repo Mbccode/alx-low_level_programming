f73eaf5f68db9434f10aec93e9fb76a185f0273f
Mbccode <bmkpume@yahoo.com>
2023-07-20 13:14:35 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+ * print_line - Entry point
+ * @n: integer variable
+ * Return: Null
+ */
+
+void print_line(int n)
+{
+	char c = '_';
+	int x;
+
+	for (x = 0; x < n; ++x)
+		_putchar(c);
+
+	_putchar('\n');
+}
