54b7f74274058abb60e7af4a90b92cb6fe5fb586
Mbccode <bmkpume@yahoo.com>
2023-07-20 11:35:49 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: integer variable
+ * @b: integer variable
+ * Return: on success return the product of (a*b) value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
