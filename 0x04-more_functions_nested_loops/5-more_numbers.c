5711a8561bc55d244ddb20b8051e7d18fcbbacb8
Mbccode <bmkpume@yahoo.com>
2023-07-20 14:24:36 -0700

    5-more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Return: Null
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y = 0;
+
+	while (y <= 9)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			_putchar('0' + x);
+		}
+		y++;
+		_putchar('\n');
+	}
+	_putchar('\n);
+}
