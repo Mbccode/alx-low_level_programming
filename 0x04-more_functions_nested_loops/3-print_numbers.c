aa8c2b043ad0a8dbab98fe1a79e565897b1eee64
Mbccode <bmkpume@yahoo.com>
2023-07-20 12:33:42 -0700

    Print0_9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Return: Null
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	while (x <= 9)
+	{
+		_putchar('0' + x);
+
+		x++;
+	}
+	_putchar('\n');
+}
