24768113673cfce70152d810ae53a0a3240aadb9
Mbccode <bmkpume@yahoo.com>
2023-07-20 21:33:57 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,28 @@
 
 int main(void)
 {
-	int x = 1;
+	int i;
 
-	while (x <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (x % 3 != 0 && x % 5 != 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("%d ", x);
-		}
-		else if (x % 3 == 0 || x % 5 == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (x % 3 == 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz ");
-		}
-		else if (x % 5 == 0)
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
-			printf("Buzz ");
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
 		}
-		x++;
 	}
 	printf("\n");
+
 	return (0);
 }
