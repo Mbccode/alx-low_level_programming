050bc5f0f9a7d0679b02df7bb853a97e7ddc889b
Mbccode <bmkpume@yahoo.com>
2023-07-24 14:18:40 -0700

    10 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - Entry point
  * @s: string to be converted
  *
  * Return: the int converted from the string
+ *
  */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
@@ -44,3 +46,4 @@ int _atoi(char *s)
 
 	return (n);
 }
+
