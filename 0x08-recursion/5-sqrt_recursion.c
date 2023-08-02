dfeeeb34d8fe550ad9307d3fd23dbc194216d5d7
Mbccode <bmkpume@yahoo.com>
2023-08-02 11:25:09 -0700

    Square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int ben_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - Entry point
+ * @n: int variable
+ *
+ * Return: the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (ben_recursion(n, 0));
+}
+
+/**
+ * ben_recursion - function helper
+ * @n: integer variable 
+ * @i: iterator variable
+ *
+ * Return: the square root
+ */
+
+int ben_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (ben_recursion(n, i + 1));
+}
