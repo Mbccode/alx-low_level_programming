6f6713e42ef702c117858b4ca97196a7fe5cfb5e
Mbccode <bmkpume@yahoo.com>
2023-08-02 09:50:34 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: Char variable
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
