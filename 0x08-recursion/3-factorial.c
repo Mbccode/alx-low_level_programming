48051780216615896d44f7634c51a96795065c6c
Mbccode <bmkpume@yahoo.com>
2023-08-02 11:00:39 -0700

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: int variable
+ * Return: factorial of int n on success
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1);
+}
