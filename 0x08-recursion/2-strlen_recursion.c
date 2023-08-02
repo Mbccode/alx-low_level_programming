52e83fb766f4e70a7d2a3c7d53ac8baab1b7e063
Mbccode <bmkpume@yahoo.com>
2023-08-02 10:49:54 -0700

    lenght corrected

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _strlen_recursion - Entry point
+ * _strlen_recursion - Entry point
  * @s: integer variable to be printed
  * Return: 1 on success, ortherwise 0
  *
@@ -12,5 +12,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (1) + _strlen_recursion(s + 1);
+		return ((1) + _strlen_recursion(s + 1));
 }
