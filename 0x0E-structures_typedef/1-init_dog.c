762befbc8c139d9b4188618e737a7d0c185f7533
Mbccode <bmkpume@yahoo.com>
2023-08-14 14:53:29 -0700

    Initialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - Entry poit
+ * @d: data of dog to be initialised
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
