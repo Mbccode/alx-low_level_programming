11481d9e4839d82a2600a530c6deb924ece2371e
Mbccode <bmkpume@yahoo.com>
2023-08-14 15:47:09 -0700

    Print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,9 +2,7 @@
 
 /**
  * print_dog - Entry point
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @d: data of dog
  */
 
 void print_dog(struct dog *d)
@@ -13,24 +11,24 @@ void print_dog(struct dog *d)
 		return;
 	if (d)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		d->name;
+		d->age;
+		d->owner;
 	}
 
 	if (name == NULL)
 		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\n", d.name);
+		printf("Name: %s\n", d->name);
 
 	if (age == NULL)
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %d\n", d.age);
+		printf("Age: %f\n", d->age);
 
 	if (owner == NULL)
-	
+
 		printf("Owner: (nil)\n");
 	else
-		printf("Owner: %s\n", d.owner);
+		printf("Owner: %s\n", d->owner);
 }
