aea153ef13f74c54fc131c4c29d3e3a297284bc1
Mbccode <bmkpume@yahoo.com>
2023-08-14 14:19:54 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
