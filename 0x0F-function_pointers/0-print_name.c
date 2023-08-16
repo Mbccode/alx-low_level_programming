e8bef41b242df3cefac88057312d96f9883f9465
Mbccode <bmkpume@yahoo.com>
2023-08-16 05:43:08 -0700

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,12 @@
  * print_name - Entry point
  * @name: name to be displayed
  * @f: pointer function
- * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
+
 	f(name);
 }
