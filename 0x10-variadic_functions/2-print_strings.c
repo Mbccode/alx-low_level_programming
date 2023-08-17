f75d40c13b0c3707b3a483d65ce650c16758afbf
Mbccode <bmkpume@yahoo.com>
2023-08-17 10:31:51 -0700

    against all odd

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,25 +6,28 @@
  * print_strings - Entry point
  * @separator: Characters to be printed
  * @n: numbers to be printed
- * Return: Null
+ * @...: A variable number of strings to be printed
+ *
+ * Description: if separator is null dont print
+ * if any of string is null print nil
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list string;
 	unsigned int count;
 
-	va_start(args, n);
+	va_start(string, n);
 
 	for (count = 0, count < n, count++)
-		if (va_arg(args, char *) == NULL)
+		if (va_arg(string, char *) == NULL)
 			printf("nil");
 		else
-			printf("%s", va_arg(args, char *);
+			printf("%s", va_arg(string, char *);
 
-		if (count < (n - 1) && separator != NULL)
+		if (count != (n - 1) && separator != NULL)
 			printf("%s", separator);
 
-	va_arg(args);
+	va_arg(string);
 	printf("\n");
 }
