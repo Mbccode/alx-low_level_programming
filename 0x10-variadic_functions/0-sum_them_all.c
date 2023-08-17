40d9f0b6f83c80c7534447129ff086fd272247ac
Mbccode <bmkpume@yahoo.com>
2023-08-17 07:38:30 -0700

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ben;
-
-	int total = 0;
+	va_list my_num;
 	unsigned int i;
+	int sum;
 
 	if (n == 0)
 		return (0);
+	sum = 0;
 
-	va_start(ben, n);
+	va_start(my_num, n);
 
 	for (i = 0; i < n; i++)
-		total += va_arg(ben, int);
+		sum += va_arg(my_num, int);
 
-	va_end(ben);
-	return (total);
+	va_end(my_num);
+	return (sum);
 }
